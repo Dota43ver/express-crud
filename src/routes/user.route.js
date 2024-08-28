40676e29fb9a231adfe81b5356624153d66e7cec
Dota43ver <facundoagueroc@gmail.com>
2024-08-28 20:36:01 -0300

    delete all test

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,8 +5,5 @@ const router = Router();
 
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
-router.get("/test", (req, res) => {
-  res.json({ msg: "test" });
-});
 
 export default router;
