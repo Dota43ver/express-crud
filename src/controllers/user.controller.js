768df51cd98d59e62c66d7d035e497f6c95130e2
Dota43ver <facundoagueroc@gmail.com>
2024-08-28 20:32:58 -0300

    test

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -11,6 +11,17 @@ const register = async (req, res) => {
   }
 };
 
+const login2 = async (req, res) => {
+  try {
+    console.log(re.body);
+    const { username, password } = req.body;
+    return res.json({ ok: true, msg: "Logged in" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Error server" });
+  }
+};
+
 const login = async (req, res) => {
   try {
     console.log(re.body);
